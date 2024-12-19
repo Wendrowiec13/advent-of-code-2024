f0d7422b430d601b54ab5e5dda9d1c1736d54fe0
Mateusz Kelner <mateuszkelner@gmail.com>
2024-12-19 02:03:58 +0100

    Solve Day 1 part 2

--- day_1/src/main.zig
+++ day_1/src/main.zig
@@ -20,31 +20,46 @@ pub fn main() !void {
     var list_2 = std.ArrayList(i32).init(allocator);
     defer list_2.deinit();
 
-    while (try line_reader.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
+    var occurrence_count_map = std.AutoHashMap(i32, i32).init(allocator);
+    defer occurrence_count_map.deinit();
 
+    while (try line_reader.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
         var split_str = std.mem.splitSequence(u8, line, "   ");
         
-        const first = split_str.first();
-        const second = split_str.next().?;
+        const first = try std.fmt.parseInt(i32, split_str.first(), 10);
+        const second = try std.fmt.parseInt(i32, split_str.next().?, 10);
+        try list_1.append(first);
+        try list_2.append(second);
 
-        try list_1.append(try std.fmt.parseInt(i32, first, 10));
-        try list_2.append(try std.fmt.parseInt(i32, second, 10));
+        if (occurrence_count_map.get(second)) |count| {
+            try occurrence_count_map.put(second, count + 1);
+        } else {
+            try occurrence_count_map.put(second, 1);
+        }
     }
 
     std.mem.sort(i32, list_1.items, {}, std.sort.asc(i32));
     std.mem.sort(i32, list_2.items, {}, std.sort.asc(i32));
 
-    var result: u32 = 0;
+    var result_1: u32 = 0;
+    var result_2: i32 = 0;
 
     for (list_1.items, list_2.items) |item_1, item_2| {
-        result += @abs(item_1 - item_2);
+        result_1 += @abs(item_1 - item_2);
+    }
+
+    for (list_1.items) |item| {
+        if (occurrence_count_map.get(item)) |count| {
+            result_2 += item * count;
+        }
     }
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Result: {d}\n", .{result});
+    try stdout.print("Result 1: {d}\n", .{result_1});
+    try stdout.print("Result 2: {d}\n", .{result_2});
 
     try bw.flush(); // don't forget to flush!
 }
